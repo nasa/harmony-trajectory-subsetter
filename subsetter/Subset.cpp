cabf386e7832a3b5f0a215668b7d79008a02902c
Josie Lyon <josie.lyon@nasa.gov>
2023-07-10 13:02:43 -0500

    Revert "Sample edit"
    This reverts commit c1ac4ba10f1aff95ecac795957e18e515941db1b.

--- subsetter/Subset.cpp
+++ subsetter/Subset.cpp
@@ -202,8 +202,6 @@ int process_args(int argc, char* argv[])
 */
 int main(int argc, char* argv[])
 {
-    std::cout << "Here there be dragons\n"; // DEBUG JSL
-
     // If process_args() returns a non-zero value,
     // then the arguments were unable to be processed.
     if (int processArgsErrorCode = process_args(argc, argv))
